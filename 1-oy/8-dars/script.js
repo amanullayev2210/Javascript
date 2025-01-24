e0c56650d3f081e369a7dfc95a0772e08b7e71a8
amanullayev2210 <karory577@gmail.com>
2025-01-24 16:37:26 +0500

    Initial commit

---
+++ 1-oy/8-dars/script.js
@@ -0,0 +1,41 @@
+let num = 12.3456;
+console.log(0xf);
+
+let n = 1.23456;
+console.log(n.toFixed(2));
+
+// let res = 5;
+console.log(parseInt("101", 2));
+
+// Math ===========
+let numbers = 5;
+console.log(Math.abs(-10));
+console.log(Math.round(5.4));
+console.log(Math.round(5.6));
+console.log(Math.floor(5.5));
+console.log(Math.ceil(5.01));
+console.log(Math.trunc(5.2));
+console.log(parseInt(5.5));
+console.log(Math.trunc(Math.random() * 10));
+console.log(Math.min(2, 3, 1));
+console.log(Math.max(3, 5, 6, 10));
+
+let obj = {
+  name: "Sardorbek",
+  title: {
+    work: "AyTi",
+  },
+};
+
+console.log(obj.name);
+console.log(obj.title.work.status?.data);
+
+
+// ?.
+// ?:
+// ==
+// ===
+
+// home work  ===========
+console.log("home work start");
+// Home work - darsda utilganlarni takrorlash, savol va javob.
