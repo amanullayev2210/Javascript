332aca91d2a0fc6ed02284c8b2635875b2ff9fdc
amanullayev2210 <karory577@gmail.com>
2025-02-09 11:06:58 +0500

    Initial commit

---
+++ 1-oy/11-dars/script.js
@@ -0,0 +1,28 @@
+console.log(1 + 1) //2
+console.log(11 + "3"); //113
+console.log("3" + 1); //113
+console.log(11 - 1 + "1"); //101
+console.log(11 - "1" + 1); //11
+
+let num = 5;
+console.log(num.toFixed(2)); // 5.12
+console.log(num = 4); // 4
+
+let namee = "Anvar"
+console.log(namee.startsWith("A")); // true
+console.log(namee.replace("A", "a")); // anvar
+console.log(namee.includes("A")); // true
+console.log(namee.search("A")); //return 0
+console.log(namee.at(4)); // r str work
+console.log(namee.charAt()); // A
+
+
+
+
+
+
+
+
+
+
+
---
+++ 1-oy/13-dars/script.js
@@ -0,0 +1,123 @@
+// ******** Imtihon kuni
+
+// let arr = [
+//   "olma",
+//   "anjit",
+//   "olcha",
+//   ["kivi", "banan", ["bexi", "gilos", "apelsin"]],
+// ];
+
+// console.log(arr[3][arr.length - 2][1]);
+
+// Anavar 20
+
+// let arr = ["olma", "olcha", "gilos", "banan"]
+// console.log(arr[2][4]);
+
+// Kamronbek 20
+
+// let str = "Sarodorbek";
+// console.log(str[str.length - 1].toUpperCase());
+// console.log(str.replace("k", "K"));
+
+// Shaxzodbek 0
+
+// let str = "gilos";
+
+// if(){
+//     console.log(true);
+// }
+
+// Anvar 20
+
+// let str = "giols";
+
+// console.log(str.includes("o"));
+
+// if (str.includes("o")) {
+//   console.log(true);
+// }
+
+// Kamronbek 0
+
+// let obj = {
+//   name: "Shaxzod",
+//   age: 13,
+//   "first name": "To'liq isim",
+// };
+
+// let name = "age";
+
+// console.log(obj[name]);
+// console.log(obj["name"]);
+
+// Kamron 30
+
+// shaxzodbek 0
+
+// console.log("" === 0);
+
+
+// Anvar return 82
+// Shaxzod return 1
+// Kamronbek return 54// let arr = [
+//   "olma",
+//   "anjit",
+//   "olcha",
+//   ["kivi", "banan", ["bexi", "gilos", "apelsin"]],
+// ];
+
+// console.log(arr[3][arr.length - 2][1]);
+
+// Anavar 20
+
+// let arr = ["olma", "olcha", "gilos", "banan"]
+// console.log(arr[2][4]);
+
+// Kamronbek 20
+
+// let str = "Sarodorbek";
+// console.log(str[str.length - 1].toUpperCase());
+// console.log(str.replace("k", "K"));
+
+// Shaxzodbek 0
+
+// let str = "gilos";
+
+// if(){
+//     console.log(true);
+// }
+
+// Anvar 20
+
+// let str = "giols";
+
+// console.log(str.includes("o"));
+
+// if (str.includes("o")) {
+//   console.log(true);
+// }
+
+// Kamronbek 0
+
+// let obj = {
+//   name: "Shaxzod",
+//   age: 13,
+//   "first name": "To'liq isim",
+// };
+
+// let name = "age";
+
+// console.log(obj[name]);
+// console.log(obj["name"]);
+
+// Kamron 30
+
+// shaxzodbek 0
+
+// console.log("" === 0);
+
+
+// Anvar return 82
+// Shaxzod return 1
+// Kamronbek return 54
