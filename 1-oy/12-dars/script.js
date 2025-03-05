33f44714a18325c96cd848abb3fd5e3014cbc677
amanullayev2210 <karory577@gmail.com>
2025-03-05 14:44:59 +0500

    update

---
+++ 1-oy/12-dars/script.js
@@ -0,0 +1,27 @@
+// Imtihonga tayyorgarlik
+
+console.log(1 + 1) //2
+console.log(11 + "3"); //113
+console.log("3" + 1); //113
+console.log(11 - 1 + "1"); //101
+console.log(11 - "1" + 1); //11
+
+let num = 5;
+console.log(num.toFixed(2)); // 5.12
+console.log(num = 4); // 4
+
+let namee = "Anvar"
+console.log(namee.startsWith("A")); // true
+console.log(namee.replace("A", "a")); // anvar
+console.log(namee.includes("A")); // true
+console.log(namee.search("A")); //return 0
+console.log(namee.at(4)); // r str work
+console.log(namee.charAt()); // A
+console.log(namee.charAt(2)); // V
+console.log(namee.toString()); // Anvar
+console.log(namee.lastIndexOf(1)); // -1
+console.log(namee.at()); // A
+console.log(namee.at(2)); // v
+console.log(namee[2]); // V
+console.log(namee.charAt(-2)); // string  
+console.log(namee.at(-2)); // 2
